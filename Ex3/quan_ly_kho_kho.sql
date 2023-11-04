491faa2057dd5af29dd547f08a424551f62e4606
Anhpham2802 <anhcherry2802@gmail.com>
2023-11-04 22:31:53 +0700

    homework 1/11

---
+++ Ex3/quan_ly_kho_kho.sql
@@ -0,0 +1,63 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: quan_ly_kho
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `kho`
+--
+
+DROP TABLE IF EXISTS `kho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kho` (
+  `id` int NOT NULL,
+  `maKho` varchar(45) NOT NULL,
+  `tenSP` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idKho_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kho`
+--
+
+LOCK TABLES `kho` WRITE;
+/*!40000 ALTER TABLE `kho` DISABLE KEYS */;
+INSERT INTO `kho` 
+VALUES 
+(1,'A01','Kho A1','Hà Nội','2023-01-01 00:00:00','2023-03-11 00:00:00'),
+(2,'A02','Kho A2','Đà Nẵng','2023-01-01 00:00:00','2023-03-11 00:00:00'),
+(3,'A03','Kho A3','TP HCM','2023-01-01 00:00:00','2023-03-11 00:00:00'),
+(4,'A04','Kho A4','Thanh Hóa','2023-01-01 00:00:00','2023-03-11 00:00:00'),
+(5,'A05','Kho A5','Pleiku','2023-01-01 00:00:00','2023-03-11 00:00:00'),
+(6,'A06','Kho A6','Nha Trang','2023-01-01 00:00:00','2023-03-11 00:00:00');
+/*!40000 ALTER TABLE `kho` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-04 21:52:12
